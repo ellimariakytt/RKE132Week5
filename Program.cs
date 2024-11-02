98299f4470d744cb7b634b2d72fdb99928ee31a8
Elts <elli-maria.kutt@tktk.ee>
2024-11-02 16:49:55 +0200

    add week5 code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//AND - &&
+
+//true && true --> true
+//true && false --> false
+//false && false --> false
+//false && true --> true
+
+//OR - || (pipes)
+//true || true --> true
+//true || false --> true
+//false || true --> false
+//false || false --> false
+
+
+
